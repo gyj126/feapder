318344585793940eed8579dda143e615174e588c
gaoyunjian <yunjian.gao@flashforge.com>
2026-04-07 15:11:53 +0800

    告警成功率阈值从硬编码50%移入配置文件，以支持自定义;相关日志调整

--- feapder/templates/project_template/setting.py
+++ feapder/templates/project_template/setting.py
@@ -175,7 +175,8 @@
 # # 时间间隔
 # WARNING_INTERVAL = 3600  # 相同报警的报警时间间隔，防止刷屏; 0表示不去重
 # WARNING_LEVEL = "DEBUG"  # 报警级别， DEBUG / INFO / ERROR
-# WARNING_FAILED_COUNT = 1000  # 任务失败数 超过WARNING_FAILED_COUNT则报警
+# WARNING_FAILED_COUNT = 1000  # 失败请求数 超过WARNING_FAILED_COUNT则报警
+# WARNING_SUCCESS_RATE = 0.5  # 请求成功率低于WARNING_SUCCESS_RATE则报警
 #
 # LOG_NAME = os.path.basename(os.getcwd())
 # LOG_PATH = "log/%s.log" % LOG_NAME  # log存储路径
